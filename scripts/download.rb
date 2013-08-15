ff30c5b2a1a06a4b61e35b7c23bd30da34876217
Aditya Ramesh <_@adityaramesh.com>
2013-08-14 22:10:14 -0700

    Improved framerate.

--- scripts/download.rb
+++ scripts/download.rb
@@ -2,9 +2,9 @@
 
 require 'rake'
 
-if ARGV.length != 2
-	puts "Usage: ./" + $PROGRAM_NAME + " <source> <framerate>"
-	puts "Example: ./" + $PROGRAM_NAME + " CAM123 60"
+if ARGV.length != 1
+	puts "Usage: ./" + $PROGRAM_NAME + " <source>"
+	puts "Example: ./" + $PROGRAM_NAME + " CAM123"
 	exit
 end
 
@@ -12,7 +12,7 @@ fn = nil
 l = FileList["out/raw/data*"].sort.last
 
 if (l.nil?)
-	fn = "data_0"
+	fn = "data_1"
 else
 	s = l[/\d+/]
 	if (s.nil?)
@@ -24,4 +24,6 @@ else
 	fn = "data_#{n}"
 end
 
-`curl -v -0 -A "GTS VideoClient/2.5" -G -d "source=#{ARGV[0]}&framerate=#{ARGV[1]}" http://webvideoserv.deldot.gov/video.jpg > out/raw/#{fn}`
+# The maximum framerate that is accepted by the server is 1000. Although we will
+# not actually get 1000 frames per second, the frame rate will be more steady.
+`curl -v -0 -A "GTS VideoClient/2.5" -G -d "source=#{ARGV[0]}&framerate=1000" http://webvideoserv.deldot.gov/video.jpg > out/raw/#{fn}`
